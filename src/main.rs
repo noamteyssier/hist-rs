dae608871c26752da1876ab17913a293f983c978
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-10-22 10:03:16 -0700

    refactor: move printing to serialization with serde

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 use anyhow::Result;
+use bstr::io::BufReadExt;
 use hashbrown::HashMap;
-use std::{
-    collections::BTreeMap,
-    io::{BufRead, BufReader, stdin},
-};
+use std::io::{BufReader, stdin};
 
 fn main() -> Result<()> {
     let input = stdin();
     let mut reader = BufReader::new(input);
-    let mut bufstr = String::default();
-    let mut map: HashMap<String, usize> = HashMap::new();
+    let mut map: HashMap<Vec<u8>, usize> = HashMap::new();
 
-    while reader.read_line(&mut bufstr)? > 0 {
-        let key = bufstr.trim();
-        if key.is_empty() {
-            continue;
-        }
-        *map.entry_ref(key).or_default() += 1;
-        bufstr.clear();
-    }
+    reader.for_byte_line(|line: &[u8]| {
+        *map.entry_ref(line).or_default() += 1;
+        Ok(true)
+    })?;
 
-    let btree = BTreeMap::from_iter(map);
-    btree.into_iter().for_each(|(key, value)| {
-        println!("{}\t{}", value, key);
-    });
+    let mut collection = map.into_iter().collect::<Vec<_>>();
+    collection.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+
+    let out_handle = std::io::stdout();
+    let mut writer = csv::WriterBuilder::new()
+        .delimiter(b'\t')
+        .from_writer(out_handle);
+    collection
+        .into_iter()
+        .try_for_each(|(key, value)| -> Result<()> {
+            let record: (usize, &str) = (value, std::str::from_utf8(&key)?);
+            writer.serialize(&record)?;
+            Ok(())
+        })?;
 
     Ok(())
 }
